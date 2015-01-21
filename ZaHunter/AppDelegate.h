cf09747070926bb818a8c552cc0ded7fdca35ecb
Syed Amaanullah <syedamaanullah@Syeds-MacBook-Pro.local>
2015-01-21 13:59:33 -0600

    Initial Commit

---
+++ ZaHunter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZaHunter
+//
+//  Created by Syed Amaanullah on 1/21/15.
+//  Copyright (c) 2015 Syed Amaanullah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
